5ae33ec7a9a550800efa5ee8af5d8041867e6b3f
ellikarg <>
2023-07-13 14:14:14 +0000

    set up the three basic functions with input fields

--- run.py
+++ run.py
@@ -14,6 +14,43 @@ SHEET = GSPREAD_CLIENT.open('Travel_Cost_Index')
 
 tci = SHEET.worksheet('TCI')
 
-data = TCI.get_all_values()[0]
+data = tci.get_all_values()
 
-print(data)
+def vacation_days():
+    """
+    Ask for the days that the user has available
+    """
+    days = input('How many days do you have available?\n')
+    return days
+
+
+def vacation_country():
+    """
+    Ask for the country the user wants to travel to
+    """
+    country = input('Where would you like to travel to?\n')
+    return country
+
+
+def vacation_level():
+    """
+    Ask for the level of adventure/comfort that the user would like to experience
+    """
+    print('\nWhich level of adventure/comfort do you want to experience?\n')
+    print('Please choose a letter from the following options:')
+    print("a: I'll travel like a backpacker\nb: I'll want some fancy hotels and food once in a while\nc: I'm all luxury\nd: I'm fed up with tourism - I want to live like a local!\n")
+    level = input('Please choose one of the options by inserting the according lowercase letter:\n')
+    return level
+
+
+def main():
+    """
+    Call all the functions above
+    """
+    vacation_days()
+    vacation_country()
+    vacation_level()
+
+
+print("Happy to see you at the Travel Cost Calculator!")
+main()
