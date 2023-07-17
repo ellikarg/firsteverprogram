9857868e69a2a87e3c200f8c4d4c764c95cdda34
ellikarg <>
2023-07-17 10:42:47 +0000

    feat: add requirements for deployment

--- run.py
+++ run.py
@@ -26,15 +26,13 @@ def vacation_days():
     Validates the data that the user inserts.
     """
     while True:
-        days = input('\n\n***\nHow many days do you have available? Insert here: ')
-
+        days = input('\n\n***\nHow many days do you have available? \
+Insert here:\n')
         try:
             days = int(days)
-
         except ValueError:
             print('\nThat did not work, please insert a whole number!')
-            print("Example: Insert '21' for three weeks")
-
+            print('Example: Insert "21" for three weeks')
         else:
             print('>>> Thank you for your input!')
             break
@@ -48,10 +46,12 @@ def vacation_country():
     """
     global country
     global country_list
-    country = input('\n\n***\nWhere would you like to travel to? Insert a country: ')
+    country = input('\n\n***\nWhere would you like to travel to? \
+Insert a country:\n')
     converted_country = country.lower()
     country = converted_country
     validate_country()
+    
     return country
 
 
@@ -62,22 +62,28 @@ def validate_country():
     """
     global country
     global country_list
+    
     if country in country_list:
-        print(f'>>> Thanks, the country {country.capitalize()} is in my database!')
+        print(f'>>> Thanks, the country {country.capitalize()} is in my \
+database!')
         time.sleep(1)
     else:
         search_country()
+
     return
 
 
 def search_country():
     """
-    Gives the user two options to search for the country she/he wants to travel to.
+    Gives the user two options to search for the country she/he wants to
+    Travel to.
     """
     global country
     global country_list
 
-    print(f'\n>>> Oh no, I cannot find the country {country}. Either it is because of a typo or you named a country that is not in my database of 132 available countries.')
+    print(f'\n>>> Oh no, I cannot find the country {country}. Either it is \
+because of a typo or you named a country that is not in my database \
+of 132 available countries.')
     search_country_options = """
     \nWhat do you want to do now?
     \n1: Search by regions
@@ -85,7 +91,8 @@ def search_country():
     print(search_country_options)
 
     while True:
-        no_country = input('\nPlease choose one of the options above (insert 1 or 2): ')
+        no_country = input('\nPlease choose one of the options above \
+(insert 1 or 2):\n')
         if no_country == '1':
             search_country_by_region()
             break
@@ -108,33 +115,32 @@ def search_country_by_region():
     regions_list = regions.row_values(1)
     print('\nPlease choose from the regions below:')
     print()
-            
     for value in regions_list:
         print(value.title())
 
     while True:
-        regions_input = input('\nInsert a region to see all the countries listed in it: ')
+        regions_input = input('\nInsert a region to see all the countries \
+listed in it:\n')
         converted_regions_input = regions_input.lower()
-        
         if converted_regions_input in regions_list:
             regions_column = regions.find(converted_regions_input)
             regions_column_values = regions.col_values(regions_column.col)[1:]
             print()
-
             for value in regions_column_values:
                 print(value.title())
-                        
             while True:
-                country_from_region = input('\nPlease insert one of the countries listed above: ')
+                country_from_region = input('\nPlease insert one of the \
+countries listed above:\n')
                 converted_country_from_region = country_from_region.lower()
-
                 if converted_country_from_region in country_list:
                     break
                 else:
-                    print("\nThat did not work, maybe it's easier to use the copy and paste function than typing it out!\n")
-            break           
+                    print("\nThat did not work, maybe it's easier to use \
+the copy and paste function than typing it out!\n")
+            break
         else:
-            print("\nThat did not work, maybe it's easier to use the copy and paste function than typing it out!\n")
+            print("\nThat did not work, maybe it's easier to use the copy \
+and paste function than typing it out!\n")
 
     country = converted_country_from_region
     validate_country()
@@ -144,25 +150,25 @@ def search_country_by_region():
 
 def search_country_by_list():
     """
-    Gives the user the option to search a country from the list of all 132 countries available.
+    Gives the user the option to search a country from the list of all 132
+    Countries available.
     """
     global country
     global country_list
-    
-    print()
 
+    print()
     for value in country_list:
         print(value.title())
-    
-    while True:
-        country_from_list=input('\nPlease choose one of the countries listed above: ')
-        converted_country_from_list = country_from_list.lower()
 
+    while True:
+        country_from_list = input('\nPlease choose one of the countries \
+listed above:\n')
+        converted_country_from_list = country_from_list.lower()
         if converted_country_from_list in country_list:
             break
         else:
-            print("\nThat did not work, maybe it's easier to use the copy and paste function than typing it out!\n")
-
+            print("\nThat did not work, maybe it's easier to use the \
+copy and paste function than typing it out!\n")
 
     country = converted_country_from_list
     validate_country()
@@ -172,12 +178,13 @@ def search_country_by_list():
 
 def vacation_level():
     """
-    Asks for the level of adventure/comfort that the user would like to experience and
-    Validates the data that the user inserts.
+    Asks for the level of adventure/comfort that the user would like
+    To experience and validates the data that the user inserts.
     """
-    options=['a', 'b', 'c', 'd']
-    print('\n\n***\nWhich level of adventure/comfort do you want to experience?')
-    vacation_level_options="""
+    options = ['a', 'b', 'c', 'd']
+    print('\n\n***\nWhich level of adventure/comfort do you want to \
+experience?')
+    vacation_level_options = """
 Please choose a letter from the following options:
 a: I'll travel like a backpacker
 b: I'll want some comfy hotels and costly food - but only once in a while
@@ -186,12 +193,13 @@ d: I'm fed up with tourism - I want to live there like a local!"""
     print(vacation_level_options)
 
     while True:
-        level=input(
-            '\nPlease choose one of the options (a, b, c or d): ')
+        level = input(
+            '\nPlease choose one of the options (a, b, c or d):\n')
         if level not in options:
-            print("\nThat did not work, please insert a, b, c or d.")
+            print('\nThat did not work, please insert a, b, c or d.')
         else:
             break
+
     return level
 
 
@@ -209,12 +217,13 @@ def get_tci(level_input):
     print('\n>>> Thank you for your request! I have all the data I need now.')
     time.sleep(1)
     print('\nYour Travel-Cost-Index is being calculated...')
-    country_cell=tci.find(country)
-    country_row=country_cell.row
-    level_cell=tci.find(level_input)
-    level_col=level_cell.col
-    country_level=tci.cell(country_row, level_col).value
+    country_cell = tci.find(country)
+    country_row = country_cell.row
+    level_cell = tci.find(level_input)
+    level_col = level_cell.col
+    country_level = tci.cell(country_row, level_col).value
     time.sleep(3)
+
     return country_level
 
 
@@ -226,16 +235,21 @@ def get_budget(tci_user, days_input):
     """
     global country
     print('\nYour Travel-Cost-Index is ready:')
-    budget="{:.2f}".format(round(float(tci_user), 2) * days_input)
-    print(f'\nYou will need {tci_user} € per day as soon as you are in {country}. For your travel of {days_input} days you will need a budget of {budget} €!')
+    budget = "{:.2f}".format(round(float(tci_user), 2) * days_input)
+    print(f'\nYou will need {tci_user} € per day as soon as you are in \
+{country}. For your travel of {days_input} days you will need a \
+budget of {budget} €!')
     print()
     print('_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_')
     print('\nThank you for using the Travel Cost Calculator!')
-    print('\nRemember this is only an estimation based on data from other travellers and can vary from individual to individual.')
-    print('\nIf you want to check out the origin of my database, please have a look at those two websites:')
+    print('\nRemember this is only an estimation based on data from other \
+travellers and can vary from individual to individual.')
+    print('\nIf you want to check out the origin of my database, please \
+have a look at those two websites:')
     print('\nhttps://www.budgetyourtrip.com/africa/rankings')
     print('https://livingcost.org/cost')
     print()
+
     return
 
 
@@ -244,15 +258,18 @@ def main():
     Calls all the functions above.
     """
     days_input = vacation_days()
-    country_input=vacation_country()
+    country_input = vacation_country()
     level_input = vacation_level()
     tci_user = get_tci(level_input)
     get_budget(tci_user, days_input)
 
 
 print('\nHappy to see you at the Travel Cost Calculator!')
-print('\nWith this calculator you can estimate the budget you will need when travelling within a specific country.')
-print('\nAll I need to know from you is how many days you have available, to which country you want to go to and at which level of comfort you want to travel.')
+print('\nWith this calculator you can estimate the budget you will need \
+when travelling within a specific country.')
+print('\nAll I need to know from you is how many days you have available, \
+to which country you want to go to and at which level of comfort you \
+want to travel.')
 print("\nLet's get started!")
 print('-------------------------------------------------')
 main()
